5eae8c968a93d9fdc0fc01c29fb6e4f8358f6218
Nikita Yadav <52972118+nikitay171@users.noreply.github.com>
2020-08-02 22:08:37 +0530

    Create Day 3-Conditional.py

---
+++ Day 3-Conditional.py
@@ -0,0 +1,18 @@
+Given an integer, , perform the following conditional actions:
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+------------------------------------------
+n = int(input())
+if n % 2 == 0:
+    if n in range(2,6):
+        print("Not Weird")
+    elif n in range(6,21):
+        print("Weird")
+    else:
+        print("Not Weird")
+else:
+    print("Weird")
